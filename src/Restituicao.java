4a8ad859ca972a1e850ff0000988af79edb698ca
joaoMoraes-git <joaoguiilhermee06@gmail.com>
2026-06-30 00:59:28 -0300

    Casa de restituição

--- src/Restituicao.java
+++ src/Restituicao.java
@@ -1,2 +1,15 @@
-public class Restituicao {
+public class Restituicao implements TipoCasa {
+
+    @Override
+    public void acao(Jogador jogador) {
+        double valorGanho = jogador.saldo * 0.10;
+        jogador.saldo += valorGanho;
+
+        System.out.println(jogador.getNome() + " caiu na casa de Restituição e recebeu um valor de " + valorGanho + " do banco");
+    }
+
+    @Override
+    public String toString() {
+        return "Restituicao";
+    }
 }
